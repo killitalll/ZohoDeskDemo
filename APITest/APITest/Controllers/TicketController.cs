c38f29c623ff0aa00bbb9b5ed6f1808a14c06486
Unknown <arad18@student.monash.edu>
2017-07-24 19:37:43 +0200

    Removing RestAPI and replacing it with APITest

---
+++ APITest/APITest/Controllers/TicketController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using APITest.Models;
+using RestSharp;
+
+namespace APITest.Controllers
+{
+    public class TicketController : ApiController
+    {
+        public Ticket[] Get()
+        {
+            return new Ticket[]
+            {
+            
+
+
+
+
+
+
+
+
+           
+            });
+        }
+        }
+    }
+}
