deb3506af990cdd8daf472e8a2ec868aa5dc9edf
Unknown <arad18@student.monash.edu>
2017-07-24 18:00:57 +0200

    Beggining of my first demo

---
+++ RestAPI1/RestAPI1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestAPI1.MvcApplication" Language="C#" %>
---
+++ RestAPI1/RestAPI1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestAPI1.Startup))]
+namespace RestAPI1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
